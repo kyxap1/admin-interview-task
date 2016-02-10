ae13d6987f2b21435545c8ced8a018f1ed975381
Alexander Kyxap <kyxap@pro-manage.net>
2016-02-10 15:55:58 +0300

    Little fix

--- runme.sh
+++ runme.sh
@@ -10,7 +10,7 @@ LANG=C
 PATH="/bin:/usr/sbin:/usr/bin:/sbin:/usr/local/bin:/usr/local/sbin"
 DATE=$(date +%F)
 
-WORKDIR="/home/http/nest.pro-manage.net/html/krasnodar/task"
+WORKDIR=~/tmp/task
 CONFDIR="$WORKDIR/data"
 TMPLDIR="$WORKDIR/tmpl"
 JINJA_TMPL="$TMPLDIR/nginx.j2"
@@ -25,7 +25,7 @@ LOG="$WORKDIR/process.log"
 print_error() { echo "$@" >&2; write_log "[ERROR] $@"; exit 1; }
 print_info()  { echo "$@"; write_log "[INFO] $@" || exit 1; }
 write_log()   { echo "$(date -R -u) $@" >> "$LOG"; }
-catf()	      { [[ -f $@ ]] && grep -vE "^(#|$|\s)" "$@" || print_error "File can not be read: $@"; }
+catf()	      { [[ -f $@ ]] && grep -v '^\s*\(#\|$\)' "$@" || print_error "File can not be read: $@"; }
 
 #########################
 
@@ -34,7 +34,7 @@ catf()	      { [[ -f $@ ]] && grep -vE "^(#|$|\s)" "$@" || print_error "File can
 
 #########################
 
-#print_info "Started generation"
+print_info "Started generation"
 
 [[ -d $WORKDIR ]] || print_error "No source dir: $WORKDIR"
 
@@ -64,4 +64,4 @@ do
 
 done < <( catf "$DOMAINS_LIST" ) > conf/vhosts.conf
 
-#print_info "Finished generation"
+print_info "Finished generation"
